d6faf132152092403e6f325875520368caccc0f7
kegma1 <61615726+kegma1@users.noreply.github.com>
2023-06-22 21:08:15 +0200

    da sir vi at den e done

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 use macroquad::prelude::*;
 
 struct Block {
-    w: f32,
-    x: f32,
+    w: f64,
+    x: f64,
     v: f64,
-    m: f32,
+    m: f64,
+    x_constraint: f64,
     is_visible: bool
 }
 
 impl Block {
-    fn new(w: f32, x: f32, v: f64, m: f32) -> Block {
-        Block { w, x , v, m, is_visible: true}
+    fn new(w: f64, x: f64, v: f64, m: f64, x_constraint: f64) -> Block {
+        Block { w, x , v, m, is_visible: true, x_constraint }
     }
 
     fn draw(&self) {
-        draw_rectangle(self.x, screen_height() - (self.w + 50.0), self.w, self.w, BLACK);
+        let x = self.x_constraint.max(screen_width().min(self.x as f32) as f64) as f64;
+
+        draw_rectangle(x as f32, screen_height() - (self.w + 50.0) as f32, self.w as f32, self.w as f32, BLACK);
+        // draw_rectangle(self.x as f32, screen_height() - (self.w + 50.0) as f32, self.w as f32, self.w as f32, BLUE);
     }
 
     fn update(&mut self) {
-        self.x += self.v as f32
+        self.x += self.v
     }
 
     fn collide(&self, other: &Block) -> f64 {
@@ -39,10 +43,10 @@ impl Block {
 
 #[macroquad::main("pi_count")]
 async fn main() {
-    let digit = 6.0;
-    let time_steps = 6600;
-    let mut block1 = Block::new(30.0, 90.0, 0.0, 1.0);
-    let mut block2 = Block::new(60.0, 600.0 , -0.5_f64/time_steps as f64, block1.m * 100.0_f32.powf(digit));
+    let digit = 8.0;
+    let time_steps = 1000000;
+    let mut block1 = Block::new(30.0, 90.0, 0.0, 1.0, 0.0);
+    let mut block2 = Block::new(60.0, 200.0 , -0.5_f64/time_steps as f64, block1.m * 100.0_f64.powf(digit), 30.0);
     let mut count = 0;
     loop {
         clear_background(WHITE);
@@ -70,10 +74,10 @@ async fn main() {
         block1.draw();
         block2.draw();
 
-        if block1.x >= screen_width() {
+        if block1.x >= screen_width().into() {
             block1.is_visible = false
         }
-        if block2.x >= screen_width() {
+        if block2.x >= screen_width().into() {
             block2.is_visible = false
         }
 
